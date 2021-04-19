5a13873cf101295f56400e19839a42bd802f1abf
KeslieBox <kesliebox@gmail.com>
2021-04-19 03:56:17 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,26 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # below syntax produces array of bird objects and each object will only have 
+    # the id, name and species values, leaving out everything else:
+    render json: birds, only: [:id, :name, :species]
+    # or the reverse would be: 
+    # render json: birds, except: [:created_at, :updated_at]
+
+    # The only and except keywords are actually parameters 
+    # of the to_json method, obscured by that magic. This renders same as above:
+    #  render json: birds.to_json(except: [:created_at, :updated_at])
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # error handling for if the bird is nil
+    if bird
+      # works for our single bird hash
+      render json: bird.slice(:id, :name, :species)
+      #  or: render json: { id: bird.id, name: bird.name, species: bird.species }
+      # jsonified into this JSON js object notation: "{\"id\":1,\"name\":\"Black-Capped Chickadee\",\"species\":\"Poecile Atricapillus\"}"
+    else
+      render json: { message: "Bird not found" }    
+    end
 end
